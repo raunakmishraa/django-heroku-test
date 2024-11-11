bf48f9f6828d3415accb59bbed5533e607ce4bf7
raunakmishraa <raunakraunak077@gmail.com>
2024-11-11 23:20:08 +0545

    heroku static

--- support/support/settings.py
+++ support/support/settings.py
@@ -128,7 +128,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT=os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
